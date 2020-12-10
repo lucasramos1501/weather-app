a676440d6e2afb852ff2f611f26d1ab44b54d041
Aluno Preparasaj <aluno@preparasaj.local>
2020-12-10 10:23:41 -0300

    start project

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Image, ImageBackground, TouchableOpacity} from 'react-native';
+import styled from 'styled-components'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <Container>
+        <ImageBG source={{  uri: "https://picsum.photos/200/300" }}>
+           <InfoArea >
+           <InfoTouchableOpacity>
+              <Text>45º</Text>
+             <Text>Weather in [City]</Text>
+           </InfoTouchableOpacity>
+           </InfoArea>
+        </ImageBG>
+      </Container>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View `
+width: 100%;
+height: 100%;
+align-items: center;
+justify-content: center;
+`
+const ImageBG = styled.ImageBackground `
+width: 100%;
+height: 100%;
+align-items: center;
+justify-content: center;
+`
+const InfoArea = styled.View `
+width: 80%;
+height: 80%;
+background-color: #fff;
+border-radius: 10px;
+`
+const InfoTouchableOpacity = styled.TouchableOpacity`
+flex: 1;
+border-radius: 10px;
+align-items: center;
+background-color: #fff;
+padding: 10px;
+align-items: center;
+justify-content: center;
+
+`
+ 
