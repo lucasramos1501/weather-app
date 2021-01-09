8a169b061d9458f00774eb1a370c95192e34b76c
Aluno Preparasaj <aluno@preparasaj.local>
2021-01-09 11:51:29 -0300

    add module picsum-photos to background app

--- App.js
+++ App.js
@@ -1,51 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View, Image, ImageBackground, TouchableOpacity} from 'react-native';
-import styled from 'styled-components'
+import { SafeAreaView, Text, View, ActivityIndicator} from 'react-native';
+import {Picsum} from 'picsum-photos'
+import styled from 'styled-components';
 
 export default function App() {
+	const BGImage = Picsum.url();
+	
   return (
-    <SafeAreaView>
+    <SafeAreaView >
       <Container>
-        <ImageBG source={{  uri: "https://picsum.photos/200/300" }}>
-           <InfoArea >
-           <InfoTouchableOpacity>
-              <Text>45º</Text>
-             <Text>Weather in [City]</Text>
-           </InfoTouchableOpacity>
-           </InfoArea>
-        </ImageBG>
-      </Container>
+			<ImageBG source={{  uri: BGImage }}>
+				<InfoArea >
+				<InfoPress>
+					<Text>45º</Text>
+					<Text>Weather in [City]</Text>
+				</InfoPress>
+				</InfoArea>
+			</ImageBG>
+	  </Container>
     </SafeAreaView>
   );
 }
 
 const Container = styled.View `
-width: 100%;
-height: 100%;
-align-items: center;
-justify-content: center;
+	width: 100%;
+	height: 100%;
+	align-items: center;
+	justify-content: center;
 `
 const ImageBG = styled.ImageBackground `
-width: 100%;
-height: 100%;
-align-items: center;
-justify-content: center;
+	width: 100%;
+	height: 100%;
+	align-items: center;
+	justify-content: center;
 `
 const InfoArea = styled.View `
-width: 80%;
-height: 80%;
-background-color: #fff;
-border-radius: 10px;
+	width: 80%;
+	height: 80%;
+	background-color: #fff;
+	border: 0.5px solid #111;
+	border-radius: 10px;
 `
-const InfoTouchableOpacity = styled.TouchableOpacity`
-flex: 1;
-border-radius: 10px;
-align-items: center;
-background-color: #fff;
-padding: 10px;
-align-items: center;
-justify-content: center;
-
+const InfoPress = styled.TouchableOpacity`
+	flex: 1;
+	border-radius: 10px;
+	align-items: center;
+	background-color: #fff;
+	padding: 10px;
+	align-items: center;
+	justify-content: center;
 `
  
