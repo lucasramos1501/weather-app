8ee9a632195d7118268702914fc00a9d84b36c3b
Lucas Ramos <LucasRamos1501@hotmail.com>
2021-01-20 14:28:10 -0300

    trying using api

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaView, Text, View, ActivityIndicator} from 'react-native';
-import {Picsum} from 'picsum-photos'
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, Text, View, ActivityIndicator, Image} from 'react-native';
+import * as Location from "expo-location"
+import { Constants } from 'expo-constants';
+import {Picsum} from 'picsum-photos';
 import styled from 'styled-components';
+import axios from 'axios';
+
+
 
 export default function App() {
 	const BGImage = Picsum.url();
+	const [weatherData, setWeatherData] = useState();
+	const [location, setLocation] = useState();
+	const [attData, setAttData] = useState()
+	
+	useEffect(()=>{	
+		async function getWatherData(lat, long){
+			const params = {
+				access_key: "bb290c73b9905318c13ae3d9b7bf4d91",
+				query: `Santo Antonio de Jesus`
+			}
+
+			const response = await axios.get("http://api.weatherstack.com/current", {params});
+
+			setWeatherData(response.data);	
+		}
+
+		async function getLocation(){
+			await Location.requestPermissionsAsync();
+			
+			await Location.getCurrentPositionAsync({})
+				.then(response => {
+					setLocation(response.coords);
+					getWatherData(response.coords.latitude, response.coords.longitude);
+				})
+			}
+
+		getLocation();
+
+	}, [attData]);
+
+	function handleAttData(){
+		attData ? setAttData(false) : setAttData(true);
+	}
 	
   return (
-    <SafeAreaView >
+    <SafeAreaView>
       <Container>
 			<ImageBG source={{  uri: BGImage }}>
-				<InfoArea >
-				<InfoPress>
-					<Text>45º</Text>
-					<Text>Weather in [City]</Text>
+				<InfoArea >	
+				<InfoPress onPress={handleAttData}>
+					<Text>{weatherData.current.feelslike}</Text>
+					<Text>Weather in {weatherData.location.name}</Text>
+					<Text>{weatherData.location.region}</Text>
 				</InfoPress>
 				</InfoArea>
 			</ImageBG>
@@ -47,7 +86,7 @@ const InfoPress = styled.TouchableOpacity`
 	border-radius: 10px;
 	align-items: center;
 	background-color: #fff;
-	padding: 10px;
+	padding: px;
 	align-items: center;
 	justify-content: center;
 `
